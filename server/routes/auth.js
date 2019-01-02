50c076d16a0631bc8226c5c62f02dfb3eaf573d3
Justin Lyon <jml6487@gmail.com>
2019-01-02 08:53:09 -0600

    add http-errors handling

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,98 +1,96 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const createError = require('http-errors')
 const jwt = require('../utils/jwt')
-
-const throwError = (code, message) => {
-  const err = new Error(message)
-  err.statusCode = code
-  throw err
-}
 
 module.exports = db => {
   const rUsers = require('../db/objects/users')(db)
 
-  const register = (req, res) => {
+  const register = (req, res, next) => {
     const { username, email, password } = req.body
     if(!username || !email || !password) {
-      return res.status(422).send('Username, email, and password required.')
+      return next(createError(422, 'Username, email, and password required.'))
     }
 
     rUsers.queryUserByEmail(email, ['email'])
       .then(user => {
         if(user) {
-          throwError(422, 'This email is already registered.')
+          return next(createError(422, 'This email is already registered.'))
         }
         return rUsers.insertUser(req.body)
       })
       .then(id => {
-        return res.json({ id })
+        if (id) {
+          return res.json({ id })
+        }
       })
       .catch(err => {
         console.error('Fatal Error: ', err.message)
-        return res.status(err.statusCode || 500).send(err.message)
+        return next(createError(err.statusCode || 500, err.message || 'Unhandled Exception.'))
       })
   }
 
-  const login = (req, res) => {
+  const login = (req, res, next) => {
     const { email, password } = req.body
     if (!email || !password) {
-      return res.status(422).send('Email and password required.')
+      return next(createError(422, 'Email and password required.'))
     }
 
     rUsers.queryUserByEmail(email, ['id', 'username', 'email', 'password'])
       .then(user => {
         if (!user) {
-          throwError(401, 'Invalid email or password.')
+          return next(createError(401, 'Invalid email or password.'))
         }
 
         return bcrypt.compare(password, user.password)
           .then(isValid => {
             if (!isValid) {
-              throwError(401, 'Invalid email or password.')
+              return next(createError(401, 'Invalid email or password.'))
             }
 
             const token = jwt.serialize({ id: user.id, email: user.email })
             res.json({ token })
           })
           .catch(err => {
-            throw err
+            return next(createError(err.statusCode, error.message))
           })
       })
       .catch(err => {
         console.error('Error logging in. ', err.message)
-        res.status(err.statusCode || 500).send(err.message)
+        return next(createError(err.statusCode || 500, err.message || 'Unhandled Exception.'))
       })
   }
 
-  const me = (req, res) => {
+  const me = (req, res, next) => {
     const accessToken = req.headers['x-access-token']
 
     if (!accessToken) {
-      return res.status(401).send('Unauthorized.')
+      return next(createError(401, 'Unauthorized.'))
     }
 
     jwt.deserialize(accessToken)
       .then(decoded => {
         const now = Date.now()
         if(decoded.exp >= now) {
-          return res.status(401).send('Unauthorized.')
+          return next(createError(401, 'Unauthorized.'))
         }
 
         return rUsers.queryUserById(decoded.id)
       })
       .then(user => {
         if (!user) {
-          throwError(400, 'User not found.')
+          return next(createError(400, 'User not found.'))
         }
         res.json(user)
       })
       .catch(err => {
         console.error('Error: ', err.message)
-        res.status(err.statusCode || 500).send(err.message || 'Unhandled Exception.')
+        return next(createError(err.statusCode || 500, err.message || 'Unhandled Exception.'))
       })
   }
 
   router.use('/', (req, res, next) => {
+    console.log('req.originalUrl', req.originalUrl)
     console.log('req.body', req.body)
     console.log('token', req.headers['x-access-token'])
     next()
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 
+const errorHandler = (err, req, res, next) => {
+  if (err) {
+    console.log('errorHandler', {
+      statusCode: err.statusCode,
+      message: err.message
+    })
+    return res.status(err.statusCode || 500).json(err || { message: 'Unhandled Exception.' })
+  }
+  next()
+}
+
 module.exports = db => {
   const auth = require('./auth')(db)
   router.use('/auth', auth)
 
+  router.use('/', errorHandler)
   return router
 }
